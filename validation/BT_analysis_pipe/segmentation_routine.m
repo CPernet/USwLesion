38b96c45504c8909c52a0e930e76e9a21a071ca4
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-01-29 13:40:30 +0000

    multispectral

--- validation/BT_analysis_pipe/segmentation_routine.m
+++ validation/BT_analysis_pipe/segmentation_routine.m
@@ -5,9 +5,10 @@ spm_jobman('initcfg')
 % do the segmentation
 matlabbatch{1}.spm.tools.USwLtools.uswl.imgMsk = {mask};
 matlabbatch{1}.spm.tools.USwLtools.uswl.imgRef = {T1name};
-matlabbatch{1}.spm.tools.USwLtools.uswl.imgStruc = {FLAIRname};
+imgs{1} = T1name; imgs{2} = FLAIRname;
+matlabbatch{1}.spm.tools.USwLtools.uswl.imgStruc = imgs';
 matlabbatch{1}.spm.tools.USwLtools.uswl.imgOth = '';
-matlabbatch{1}.spm.tools.USwLtools.uswl.options.imgTpm = {'C:\Users\s1835343\mri_stuff\spm12\toolbox\USwLesion\eTPM\eTPM_wBG.nii'}; % {[fileparts(which('spm')) filesep 'tpm' filesep 'TPM.nii']};
+matlabbatch{1}.spm.tools.USwLtools.uswl.options.imgTpm =  {[fileparts(which('spm')) filesep 'tpm' filesep 'TPM.nii']}; % {'C:\Users\s1835343\mri_stuff\spm12\toolbox\USwLesion\eTPM\eTPM_wBG.nii'}; % {[fileparts(which('spm')) filesep 'tpm' filesep 'TPM.nii']};
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.NbGaussian = [2 2 nbGaussian 2 2 0 2]; 
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.tpm4lesion = affectedtissue;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biasreg = 0.001;
@@ -15,10 +16,5 @@ matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biasfwhm = 60;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biaswr = [1 1];
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.ICVmsk = 1;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.mrf = 2;
-
-% clean up
-matlabbatch{2}.spm.tools.USwLtools.USwLutils.FxLesMsk.fnMsk(1) = cfg_dep('US with lesion: c3 image', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','segmImg', '.','c3'));
-matlabbatch{2}.spm.tools.USwLtools.USwLutils.FxLesMsk.options.minVol = 10;
-matlabbatch{2}.spm.tools.USwLtools.USwLutils.FxLesMsk.options.fnOth = '';
 
 out = spm_jobman('run', matlabbatch);
