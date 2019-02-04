9e000405c17060bc0e64189d5ae0b18de8259fb6
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-02-04 12:06:43 +0000

    delete stuff

--- validation/BT_analysis_pipe/test_batch.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'C:\Users\s1835343\mri_stuff\spm12\toolbox\USwLesion\validation\BT_analysis_pipe\test_batch_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
