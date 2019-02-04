a04161392dbce385c27e6994c4ee07abed4abf17
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-02-04 16:48:57 +0000

    fixed errors

--- validation/BT_analysis_pipe/compare2gdtruth.m
+++ validation/BT_analysis_pipe/compare2gdtruth.m
@@ -13,21 +13,17 @@ function [mJ,mHd,overlap,Dice] = compare2gdtruth(c1,c2,c3,c4,ground_truth,save_o
 save_option = 'off';
 
 %% 1st load images and check all dimension match
+       
+        c1_vol = spm_read_vols(spm_vol(c1));
+        c2_vol = spm_read_vols(spm_vol(c2));
+        c3_vol = spm_read_vols(spm_vol(c3));
+        c4_vol = spm_read_vols(spm_vol(c4));
 
-c1 = [patient_dir filesep tmp.name filesep ['segmentation_' num2str(index)] filesep 'c1kVSD.nii'];
-c2 = [patient_dir filesep tmp.name filesep ['segmentation_' num2str(index)] filesep 'c2kVSD.nii'];
-c3 = [patient_dir filesep tmp.name filesep ['segmentation_' num2str(index)] filesep 'c3kVSD.nii'];
-c4 = [patient_dir filesep tmp.name filesep ['segmentation_' num2str(index)] filesep 'c4kVSD.nii'];
-
-c1_vol = spm_read_vols(spm_vol(c1));
-c2_vol = spm_read_vols(spm_vol(c2));
-c3_vol = spm_read_vols(spm_vol(c3));
-c4_vol = spm_read_vols(spm_vol(c4));
-
-if isequal(size(c1_vol),size(c2_vol),size(c3_vol),size(c4_vol)); 
-    disp('dimensions do match');
-else disp('dimensions do not match');
-end
+        if isequal(size(c1_vol),size(c2_vol),size(c3_vol),size(c4_vol));
+            disp('dimensions do match');
+        else disp('dimensions do not match');
+        end
+    
 
 %% create binary images of c3
 
@@ -41,7 +37,7 @@ if strcmp(save_option,'on')
     spm_write_vol
 end
 
-% tumour prob > any of other classes 
+% tumour prob > any of other classes
 
 c3b = (c3_vol > c1_vol) | (c3_vol > c2_vol) | (c3_vol > c4_vol);
 
@@ -51,25 +47,27 @@ end
 
 % tumour > 99%
 
-c3c = sum(c3_vol(:)>0.99) / sum(c3_vol(:)~=0);
+c3c = c3_vol > 0.99;
 
 if strcmp(save_option,'on')
     spm_write_vol
 end
 
 
-clean c1 c2 c3 c4
+clear c1 c2 c3 c4
 
 %% now compute overlap
 
 GT = spm_read_vols(spm_vol(ground_truth));
 
 [mJ(1),mHd(1),overlap(1)] = image_overlap(c3a,GT);
-Dice(1) = 2*overlap1(1).voxel.tp / (2*overlap1(1).voxel.tp+2*overlap1(1).voxel.fp+2*overlap1(1).voxel.fn);
+Dice(1) = 2*overlap(1).voxel.tp / (2*overlap(1).voxel.tp+2*overlap(1).voxel.fp+2*overlap(1).voxel.fn);
 
-[mJ(2),mHd(3),overlap(2)] = image_overlap(c3b,GT);
-Dice(2) = 2*overlap1(2).voxel.tp / (2*overlap1(2).voxel.tp+2*overlap1(2).voxel.fp+2*overlap1(2).voxel.fn);
+[mJ(2),mHd(2),overlap(2)] = image_overlap(c3b,GT);
+Dice(2) = 2*overlap(2).voxel.tp / (2*overlap(2).voxel.tp+2*overlap(2).voxel.fp+2*overlap(2).voxel.fn);
 
 [mJ(3),mHd(3),overlap(3)] = image_overlap(c3c,GT);
-Dice(3) = 2*overlap1(3).voxel.tp / (2*overlap1(3).voxel.tp+2*overlap1(3).voxel.fp+2*overlap1(3).voxel.fn);
+Dice(3) = 2*overlap(3).voxel.tp / (2*overlap(3).voxel.tp+2*overlap(3).voxel.fp+2*overlap(3).voxel.fn);
+
+   
 
