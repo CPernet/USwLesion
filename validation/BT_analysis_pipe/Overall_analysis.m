ebc66120acaa827dfa878384bd6aea4da61cd1dc
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-03-14 16:32:35 +0000

    added bit to calculate correlation between metrics

--- validation/BT_analysis_pipe/Overall_analysis.m
+++ validation/BT_analysis_pipe/Overall_analysis.m
@@ -244,7 +244,7 @@ end
 
 %% step 4: statistically test which masks are the best and in which conditions
 
-stats_analysis(save_in)
+[Perf_data, Perf_ranked_data, Perf_adjdata, Perf_ranked_adjdata,cluster_labels] = stats_analysis(save_in);
 
 % check volumes of the new lesion masks 
 
@@ -289,11 +289,17 @@ for tumour_type = 1:2
                     c3_thresh4 = [patient_dir filesep tmp.name filesep mask_folder filesep 'c3kVSD_bigger_than_99.nii'];
                     c3_thresh5 = [patient_dir filesep tmp.name filesep mask_folder filesep 'c3kVSD_bigger_than_50.nii'];
                     c3_vol = spm_read_vols(spm_vol(c3_before));
+                    c3_vol = c3_vol>0;
                     c3_thresh1_vol = spm_read_vols(spm_vol(c3_thresh1));
+                    c3_thresh1_vol = c3_thresh1_vol > 0;
                     c3_thresh2_vol = spm_read_vols(spm_vol(c3_thresh2));
+                    c3_thresh2_vol = c3_thresh2_vol > 0;
                     c3_thresh3_vol = spm_read_vols(spm_vol(c3_thresh3));
+                    c3_thresh3_vol = c3_thresh3_vol > 0;
                     c3_thresh4_vol = spm_read_vols(spm_vol(c3_thresh4));
+                    c3_thresh4_vol = c3_thresh4_vol > 0;
                     c3_thresh5_vol = spm_read_vols(spm_vol(c3_thresh5));
+                    c3_thresh5_vol = c3_thresh5_vol > 0;
                     mask_volumes(subj_index,param_index,1) = sum(c3_vol(:));
                     mask_volumes(subj_index,param_index,2) = sum(c3_thresh1_vol(:));
                     mask_volumes(subj_index,param_index,3) = sum(c3_thresh2_vol(:));
@@ -327,5 +333,31 @@ for tumour_type = 1:2
     
 end
 
+% calculating correlation between metrics
 
+baseline_data = importdata([csv_folder filesep 'baseline_measures.csv']); baseline_data = baseline_data.data;
+dice_data = importdata([csv_folder filesep 'Dice_results.csv']); dice_data = dice_data.data;
+mHd_data = importdata([csv_folder filesep 'mHd_results.csv']); mHd_data = mHd_data.data;
+mJ_data = importdata([csv_folder filesep 'mJ_results.csv']); mJ_data = mJ_data.data;
+kappa_data = importdata([csv_folder filesep 'overlap_kappa_results.csv']); kappa_data = kappa_data.data;
+mcc_data = importdata([csv_folder filesep 'overlap_mcc_results.csv']); mcc_data = mcc_data.data;
+
+dice_data = dice_data - repmat([repmat(baseline_data(:,5),[1 6]) repmat(baseline_data(:,6),[1 6]) ],[1,5]);
+mHd_data = repmat([repmat(baseline_data(:,3),[1 6]) repmat(baseline_data(:,4),[1 6]) ],[1,5]) - mHd_data;
+mJ_data = mJ_data - repmat([repmat(baseline_data(:,1),[1 6]) repmat(baseline_data(:,2),[1 6]) ],[1,5]);
+kappa_data = kappa_data - repmat([repmat(baseline_data(:,9),[1 6]) repmat(baseline_data(:,10),[1 6]) ],[1,5]);
+mcc_data = mcc_data - repmat([repmat(baseline_data(:,7),[1 6]) repmat(baseline_data(:,8),[1 6]) ],[1,5]);
+
+[r1,t1,pval1] = Pearson(dice_data,mHd_data); %compute correlation of global metrics
+[r2,t2,pval2] = Pearson(dice_data,mJ_data); 
+[r3,t3,pval3] = Pearson(mJ_data,mHd_data); 
+[r4,t4,pval4] = Pearson(kappa_data,mcc_data); % compute correlation of local metrics
+
+pearson_dice_mHd = table(r1',t1',pval1','VariableNames',{'Dice_mHd_r','Dice_mHd_t','Dice_mHd_pval'});
+pearson_dice_mJ = table(r2',t2',pval2','VariableNames',{'Dice_mJ_r','Dice_mJ_t','Dice_mJ_pval'});
+pearson_mJ_mHd = table(r3',t3',pval3','VariableNames',{'mJ_mHd_r','mJ_mHd_t','mJ_mHd_pval'});
+pearson_kappa_mcc = table(r4',t4',pval4','VariableNames',{'kappa_mcc_r','kappa_mcc_t','kappa_mcc_pval'});
+
+correlation_results = horzcat(pearson_dice_mHd,pearson_dice_mJ,pearson_mJ_mHd,pearson_kappa_mcc);
+writetable(correlation_results,[save_in filesep 'correlation_results.csv']);
 
