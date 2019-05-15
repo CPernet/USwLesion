efef01f5600105ef87848a3fef51a61f7e72d57f
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-05-15 11:32:45 +0100

    with optional c1 c2

--- validation/BT_analysis_pipe/normalization/SSI.m
+++ validation/BT_analysis_pipe/normalization/SSI.m
@@ -1,13 +1,13 @@
 function SSIM = SSI(varargin)
 
-% computes the structural similarity index 
+% computes the structural similarity index
 % Wang et al 2004 IEEE Trans Image Process 13, 600-612
-% 
+%
 % FORMAT SSIM = SSI(image1,image2,roi)
-%        SSIM = SSI(image1,image2,roi)
+%        SSIM = SSI(image1,image2,roi,c1,c2)
 %
 % INPUT image1 and image2 are the image to process (char or matrix)
-%       roi is the region use to mask imgages 1 and 2 (ie restrict SSI to that ROI, 
+%       roi is the region use to mask imgages 1 and 2 (ie restrict SSI to that ROI,
 %       could be ICV mask for the whole brain or a smaller region)
 %       C1 and C2 (optional) are the mean and variance constant stabilizer
 %       (useful to use the same C1 and C2 when compararing multiple SSIM -
@@ -56,27 +56,23 @@ end
 % if means or variances are close to 0, the quality index is unstable, we thus need to define usefull constant
 
 if nargin == 4
-    C1 = varargin{4}; 
+    C1 = varargin{4};
     if isnumeric(C1)
         C2 = 3*C1;
     else
         error('the constant input is not a numerical value, see help')
     end
 elseif nargin == 5
-    C1 = varargin{4}; 
-    C2 = varargin{5}; 
+    C1 = varargin{4};
+    C2 = varargin{5};
     if any([isnumeric(C1) isnumeric(C2)])
         error('the constant input are not a numerical values, see help')
     end
 else
     C1 = (0.01*max([range(image1(:)) range(image2(:))])).^2; % 0.01 as in the matlab ssim function
-    C2 = (0.03*max([range(image1(:)) range(image2(:))])).^2; 
+    C2 = (0.03*max([range(image1(:)) range(image2(:))])).^2;
 end
 
 S = cov(image1(roi),image2(roi));
 SSIM = ((2*nanmean(image1(roi))*nanmean(image2(roi))+C1)*(2*S(1,2)+C2)) / ...
     (nanmean(image1(roi))^2+nanmean(image2(roi))^2+C1)*(nanvar(image1(roi))+nanvar(image2(roi))+C2);
-
-
-
-
