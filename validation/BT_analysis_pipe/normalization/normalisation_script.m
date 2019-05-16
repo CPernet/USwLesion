a3b0d4ca8d03544ed3a21f80e7d24a4c54254736
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-05-16 16:36:47 +0100

    Update normalisation_script.m

--- validation/BT_analysis_pipe/normalization/normalisation_script.m
+++ validation/BT_analysis_pipe/normalization/normalisation_script.m
@@ -112,7 +112,7 @@ end
 
 % segmentation
 spm_jobman('initcfg')
-for subject = 1:30
+ for subject = 1:30
     
     if subject < 11
         tumour_type = 2;
@@ -149,6 +149,8 @@ for subject = 1:30
     matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
     matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
     MNI_patient = spm_jobman('run', matlabbatch);
+    
+    clear matlabbatch
 
         % normalized original tumour 
     matlabbatch{1}.spm.spatial.normalise.write.subj.def = out;
@@ -159,6 +161,9 @@ for subject = 1:30
     matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 1;
     matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
     MNI_tumour = spm_jobman('run', matlabbatch);
+    
+    clear matlabbatch
+    
 end
 
 %% set a tumour inside healthy brain
@@ -200,27 +205,112 @@ for subject = 1:30
     T1w(mask) = Patient(mask);
     cd(Healthy_dir); local = dir;
     cd(local(subject+2).name)  
-    V1.fname = [pwd filesep 'T1w_with_tumour.nii'];
+    V1.fname = [pwd filesep 'T1w_with_tumour_TEST.nii'];
     spm_write_vol(V1,T1w);
 
+    %get the correct bounding box
     matlabbatch{1}.spm.util.bbox.image = {'C:\Users\s1835343\mri_stuff\BRAT\F1000\0025337\anat.nii,1'};
     matlabbatch{1}.spm.util.bbox.bbdef.fov = 'fv';
     out = spm_jobman('run', matlabbatch);
     clear matlabbatch
 
-    matlabbatch{1}.spm.spatial.normalise.write.subj.def = {[pwd filesep 'iy_anat.nii']};%F1000/iy;
+    %inverse normalise the healthy brain+tumour back into subject space
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def = {[pwd filesep 'iy_anat.nii']};
     matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {[pwd filesep 'T1w_with_tumour.nii']};%{'C:\Users\s1835343\mri_stuff\BRAT\BRATS2015_Training\LGG\brats_2013_pat0002_1\VSD.Brain.XX.O.MR_T1.54633\Scaled_VSD.nii,1'};
     matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = out{1}.bb;
     matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 1];
     matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
     matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
     spm_jobman('run', matlabbatch);
-    
     clear matlabbatch
+    
+    %inverse normalise the tumour mask into healthy patient subject space
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def = {[pwd filesep 'iy_anat.nii']};
+    matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {Tumour};
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = out{1}.bb;
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 1];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w_HV_';
+    spm_jobman('run', matlabbatch);
+    clear matlabbatch
+    
+    %inverse normalise the brain mask into healthy patient subject space
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def = {[pwd filesep 'iy_anat.nii']};
+    matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {[pwd filesep 'brain_mask.nii']};
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = out{1}.bb;
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 1];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
+    spm_jobman('run', matlabbatch);
+    clear matlabbatch
+    
+end
+
+%% segment and compute similarity 
+
+% standard SPM segmentation of wT1w_skull_stripped.nii from healthy subject
+
+for subject = 1:30
+    cd(Healthy_dir); local = dir;
+    cd(local(subject+2).name)   
+    wT1w_skull_stripped = [pwd filesep 'wT1w_skull_stripped.nii']; 
+    brain_mask = [pwd filesep 'wbrain_mask.nii']; %brain mask from healthy subject
+    wT1w_with_tumour = [pwd filesep 'wT1w_with_tumour.nii']; 
+    
+    if subject < 11
+        tumour_type = 2;
+    else
+        tumour_type = 1;
+    end
+    
+    % loop in High Grade Glioma or Low Grade Glioma
+    % --------------------------------------------
+    BRAT_dir = eval(['BRAT_dir' num2str(tumour_type)]);
+    cd(BRAT_dir); folders = dir('brats*');
+    if tumour_type == 1
+        folders = folders(1:20);
+        patient_dir = [BRAT_dir filesep folders(subject-10).name];
+    else
+        folders = folders(1:10);
+        patient_dir = [BRAT_dir filesep folders(subject).name];
+    end
+    tmp = dir([patient_dir filesep 'VSD.Brain_*more*']);
+    inv_wtVSD = [patient_dir filesep tmp.name filesep 'ww_HV_wtVSD.nii'];
+    
+    cd(Healthy_dir); local = dir;
+    cd(local(subject+2).name)
+    
+    %standard SPM segmentation of wT1w_skull_stripped.nii from healthy subject
+    standard_H =standard_spm_segment_job(wT1w_skull_stripped);
+    image_1 = [pwd filesep 'healthy_standard_segmentation' filesep 'c1wT1w_skull_stripped.nii'];
+    %[image_1{1,1}.tiss.c]
+    
+    %standard SPM segmentation of wT1w_with_tumour.nii
+    
+    standard_HT =standard_spm_segment_job(wT1w_with_tumour);
+    image_2 = [pwd filesep 'healthy_tumour_standard_segmentation' filesep 'c1wT1w_with_tumour.nii'];
+    
+    %USwL segmentation of wT1w_with_tumour.nii
+    seg_with_lesion_HT = segment_with_lesion(inv_wtVSD,wT1w_with_tumour);
+
+    %image 1 vs image 2
+    %measure similarity of whole brain
+    SSIM_1 = SSI(image_1,image_2,brain_mask,1);
+    
+    %do we get the same values per voxel?
+    rms_1 = sqrt(mean((c1image1-c1image2).^2));
+    
+    %image 1 vs image 3
+    %measure similarity of whole brain
+    SSIM_2 = SSI(image1,image3,roi,1);
+    
+    %do we get the same values per voxel?
+    rms_2 = sqrt(mean((c1image1-c1image2).^2));
 
 end
 
-%% segment and compute similarity stuff
+
+
 
 image1 --> standard SPM segmentation of wT1w_skull_stripped.nii from healthy subject
 roi --> brain_mask.nii from healthy subject
