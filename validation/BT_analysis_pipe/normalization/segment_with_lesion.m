6f65a501b7a5efd0a80276ab69b9b877585c84f5
Fatemeh Amini <s1835343@sms.ed.ac.uk>
2019-05-17 16:33:13 +0100

    Update segment_with_lesion.m

--- validation/BT_analysis_pipe/normalization/segment_with_lesion.m
+++ validation/BT_analysis_pipe/normalization/segment_with_lesion.m
@@ -1,4 +1,4 @@
-function out = segment_with_lesion(inv_wtVSD,wT1w_with_tumour)
+function out = segment_with_lesion(inv_wtVSD,wT1w_with_tumour,nbGaussian,affectedtissue)
 
 % do the segmentation
 spm_jobman('initcfg')
@@ -10,8 +10,8 @@ matlabbatch{1}.spm.tools.USwLtools.uswl.imgRef = {wT1w_with_tumour};
 matlabbatch{1}.spm.tools.USwLtools.uswl.imgStruc = '';
 matlabbatch{1}.spm.tools.USwLtools.uswl.imgOth = '';
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.imgTpm = {[fileparts(which('spm')) filesep 'tpm' filesep 'TPM.nii']};
-matlabbatch{1}.spm.tools.USwLtools.uswl.options.NbGaussian = [2 2 3 2 2 0 1]; 
-matlabbatch{1}.spm.tools.USwLtools.uswl.options.tpm4lesion = 3;
+matlabbatch{1}.spm.tools.USwLtools.uswl.options.NbGaussian = [2 2 nbGaussian 2 2 0 1]; 
+matlabbatch{1}.spm.tools.USwLtools.uswl.options.tpm4lesion = affectedtissue;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biasreg = 0.001;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biasfwhm = 60;
 matlabbatch{1}.spm.tools.USwLtools.uswl.options.bias.bias_yes.biaswr = [1 1];
